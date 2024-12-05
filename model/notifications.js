639ec51704670cdc6780e2cacf295e53be78df35
hanieh hosseini <119689812+haniehhoseini@users.noreply.github.com>
2024-12-05 16:45:14 +0330

    notification

--- model/notifications.js
+++ model/notifications.js
@@ -4,22 +4,20 @@ const { sendNotificationToUser } = require('../sockets/webSocket');
 
 // تابعی برای ارسال نوتیفیکیشن‌ها بر اساس ضریب‌های ساعت مصرف دارو
 exports.scheduleNotifications = () => {
-    cron.schedule('* * * * *', async () => { // هر دقیقه اجرا می‌شود، می‌توانید تغییر دهید
+    cron.schedule('0 * * * *', async () => { // هر ساعت در دقیقه 0 اجرا می‌شود
         const now = new Date();
         const currentTime = now.getHours() * 60 + now.getMinutes(); // زمان فعلی به دقیقه
 
         const query = 'SELECT * FROM prescription';
         const [prescriptions] = await db.connection.execute(query);
+        console.log([prescriptions]);
+        
 
         prescriptions.forEach((prescription) => {
-            const clockParts = prescription.clock.split(':');
-            const clockTimeInMinutes = parseInt(clockParts[0]) * 60 + parseInt(clockParts[1]); // تبدیل ساعت مصرف دارو به دقیقه
+            const clockTime = parseInt(prescription.clock); // زمان مصرف دارو (به ساعت)
 
-            const interval = prescription.interval; // فاصله زمانی مصرف دارو (مثلاً 6، 8 یا 24 ساعت)
-            const diff = currentTime - clockTimeInMinutes;
-
-            // بررسی اینکه آیا زمان فعلی ضریبی از فواصل زمانی مصرف دارو هست یا خیر
-            if (diff >= 0 && diff % (interval * 60) === 0) {
+            // بررسی اینکه آیا زمان فعلی ضریبی از ساعت مصرف دارو هست یا خیر
+            if (currentTime % (clockTime * 60) === 0) {
                 const message = `زمان مصرف داروی ${prescription.drug_name} رسیده است.`;
                 sendNotificationToUser(prescription.user_id, message); // ارسال نوتیفیکیشن به کاربر
             }
